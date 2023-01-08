e57f5e36ee086e190c469d1ec634ae67efde5aff
Kefa698 <kefaombongi07@gmail.com>
2023-01-08 16:13:09 +0300

    fixed

--- script/deploy_rebase.js
+++ script/deploy_rebase.js
@@ -2,7 +2,7 @@ async function main() {
     const Accumulator = await ethers.getContractFactory("Rebase")
 
     // Start deployment, returning a promise that resolves to a contract object
-    const accumulator = await Accumulator.deploy({ gasLimit: 1000000})
+    const accumulator = await Accumulator.deploy({ gasLimit: 1000})
     await accumulator.deployed()
     console.log("Contract deployed to address:", accumulator.address)
 }
